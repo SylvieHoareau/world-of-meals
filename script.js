4f09d43a03c9f404308349ffce7f96384b279a0e
SylvieHoareau <hoareausms@gmail.com>
2023-07-11 13:34:55 +0400

    Les cards s'affichent !
    Les cards s'affichent !

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const searchInput = document.getElementById("searchInput");
 const inputRange = document.getElementById("inputRange");
 const outputRange = document.getElementById("outputRange");
 const atoz = document.getElementById("atoz");
-const cardContainer = document.getElementById("cardContainer");
+const cardContainer = document.querySelector(".cardContainer");
 const card = document.getElementById("card");
 
 //Variables globales
-let mealsList = [];
+let meals = [];
 let sortMethod = "";
 
 //Récuprer les données de l'API
 async function getMeals() {
-    const response = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=b");
+    const response = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken");
     const data = await response.json();
-    mealsList = data;
-    console.log(mealsList);
+    meals = data.meals;
+    displayMeals();
 }
 
-getMeals();
+
 
 
 //Fonction pour afficher les repas
@@ -26,28 +26,23 @@ const displayMeals = () => {
     //J'efface les données existantes sur les cards
     // cardContainer.innerHTML = "";
     //Tri des données
-    mealsList
-    //Pour rechercher un repas avec la barre de recherche
-    .filter((mealsList) => {return mealsList.toLowerCase().indexOf(searchInput.toLowerCase())})
-    // .sort((m1, m2) => {
-    //     //Tri de A à Z
-    //     if (m1.slice(0,1) = "A") {
-
-    //     }
-    //     //Tri de Z à A
-    //     else if(m2.slice(0,1) = "Z") {
-
-    //     }
-
-    // })
-    .slice(0, inputRange)
-    .map((m) => {
+    meals
+    //Pour trier les données
+    .sort((m1, m2) => {
+        //Tri de A à Z
+        m1.localeCompare(m2);
+    
+    })
+    //Pour découper le tableau meals en fonction du nombre de repas
+    .slice(0, inputRange.value)
+    //Pour afficher les données sur l'UI
+    .map((meal) => {
         cardContainer.innerHTML += `
             <div class="card">
-                <h1 id="title">${m.strMeal}</h1>
-                <img src="${m.strMealThumb}" alt="${m.strMeal}" id="image">
-                <h2 id="origin">Origin : ${m.strArea}</h2>
-                <p id="instructions">${m.strInstructions}</p>
+                <h1 id="title">${meal.strMeal}</h1>
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}" id="image">
+                <h2 id="origin">Origin : ${meal.strArea}</h2>
+                <p id="instructions">${meal.strInstructions}</p>
             </div>
         `
     });
@@ -59,11 +54,9 @@ inputRange.addEventListener("input", (e) => {
     outputRange.textContent = e.target.value;
 });
 
-atoz.addEventListener("click", () => {
-
-
+//Quand on clique sur le bouton "A-Z"
+const atozSort = () => atoz.addEventListener("click", () => {
+    atoz.textContent = "Z to A";
 });
 
-
-
-displayMeals();
+getMeals();
