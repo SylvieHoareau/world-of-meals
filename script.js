88ca41782d4dc2f6a3a791f9b012befb8f8c8099
SylvieHoareau <hoareausms@gmail.com>
2023-07-28 10:29:20 +0400

    Update après relocalisation du dossier

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //Je récupére les élements de l'UI
 const searchInput = document.getElementById("searchInput");
-const btnSearch = document.getElementById("btnSearch");
 const inputRange = document.getElementById("inputRange");
 const outputRange = document.getElementById("outputRange");
 const atoz = document.getElementById("atoz");
@@ -9,16 +8,20 @@ const card = document.getElementById("card");
 
 //Variables globales
 let meals = [];
-let sortMethod = "";
-let switching = true;
+let sortMethod = true;
 
 //Récuprer les données de l'API
-async function getMeals() {
-    const response = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s");
-    const data = await response.json();
-    meals = data.meals;
-    displayMeals();
-}
+const getMeals = () => {
+    //Pour rechercher les informations dans la barre de recherche
+    let search = "chicken";
+    if (searchInput.value) search = searchInput.value;
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + search)
+        .then((response) => response.json())
+        .then((data) => {
+            meals = data.meals;
+            if (meals) displayMeals();
+        });
+};
 
 //Fonction pour afficher les repas
 const displayMeals = () => {
@@ -29,19 +32,11 @@ const displayMeals = () => {
     //Pour trier les données
     .sort((m1, m2) => {
         //Tri de A à Z
-        if(m1.strMeal < m2.strMeal) {
-            return m1.strMeal.localeCompare(m2.strMeal);
+        if(sortMethod) {
+            return m1.strMeal.toLowerCase().localeCompare(m2.strMeal.toLowerCase());
         }
-        else if(m1.strMeal > m2.strMeal) {
-            return m2.strMeal.localeCompare(m1.strMeal);
-        }
-        
-        // if (sortMethod === "atoz") {
-        //     return m1.strMeal.localeCompare(m2.strMeal);
-        // }
-        // else if (sortMethod === "ztoa") {
-        //     return m2.strMeal.localeCompare(m1.strMeal);
-        // }
+        return m2.strMeal.toLowerCase().localeCompare(m1.strMeal.toLowerCase());
+
     })
     //Pour découper le tableau meals en fonction du nombre de repas
     .slice(0, inputRange.value)
@@ -58,12 +53,6 @@ const displayMeals = () => {
     });
 }
 
-//Pour faire les recherches
-const search = () => btnSearch.addEventListener("change", () => {
-    var title = meals.strMeal;
-    title.toLowerCase().toLocalCompare(searchInput.value.toLowerCase());
-});
-
 //Pour afficher la valeur de l'input range à l'écran
 outputRange.textContent = inputRange.value;
 inputRange.addEventListener("input", (e) => {
@@ -71,24 +60,14 @@ inputRange.addEventListener("input", (e) => {
     displayMeals();
 });
 
+searchInput.addEventListener("change", getMeals);
+
 
 //Quand on clique sur le bouton "A-Z"
-const alphabeticalSort = () => atoz.addEventListener("click", () => {
-    sortMethod = atoz.id;
-    atoz.textContent = "Z to A";
-    sortMethod === "ztoa";
-    // //La sortMethod change
-    // let switching = true;
-    // if(switching = true) {
-    //     sortMethod === "ztoa";
-    //     atoz.textContent = "Z to A";
-    // }
-    // else {
-    //     sortMethod === "atoz";
-    //     atoz.textContent = "A to Z";
-    // }
-    });
-
-console.log(sortMethod);
+atoz.addEventListener("click", () => {
+    atoz.innerHTML = sortMethod? "A-Z":"Z-A";
+    sortMethod = !sortMethod;
+    displayMeals();
+});
 
 getMeals();
